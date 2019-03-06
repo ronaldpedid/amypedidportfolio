dfdccde7de63c82dacf56d4f7764a4fd84d2a563
Ronald Pedid <ronaldpedid@live.com>
2019-03-05 22:23:09 -0600

    nav fixes

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -54,9 +54,9 @@ class ArrowOpen extends Component {
       <div className={styles.arrowContainer}>
         <div onClick={this.props.onClick} className={styles.arrowUp}></div>
         <ul className={styles.navListMobile}>
-          <Link to="/"><li>Work</li></Link>
-          <Link to="/about"><li>About</li></Link>
-          <Link to={resumeUrl}><li>Resume</li></Link>
+          <Link to={urls[0]}><li>Work</li></Link>
+          <Link to={urls[1]}><li>About</li></Link>
+          <Link to={urls[2]}><li>Resume</li></Link>
         </ul>
       </div>
     )
