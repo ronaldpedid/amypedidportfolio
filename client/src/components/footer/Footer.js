1950f92536378f6e1e8c465b3120cfff0978ab84
Ronald Pedid <ronaldpedid@live.com>
2019-03-06 15:30:34 -0600

    amys changes

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,11 +6,9 @@ export default class Footer extends Component {
     const twitterUrl = 'https://twitter.com/Amy_AHHA_Moment';
     return (
       <footer className={styles.footer}>
-        <h2>&copy; 2019</h2>
-        <div className={styles.socialRow}>
-          <h2 className={styles.social}><a href="mailto:amypedid@outlook.com?subject=Website Mail"><i className="far fa-envelope"></i></a></h2>
-          <h2 className={styles.social}><a className={styles.socialLink} href={twitterUrl} target="_blank"><i className="fab fa-twitter"></i></a></h2>
-        </div>
+        <h2>&copy; COPYRIGHT 2019</h2>
+        <h2 className={styles.social}><a href="mailto:amypedid@outlook.com?subject=Website Mail"><i className="far fa-envelope"></i></a></h2>
+        <h2 className={styles.social}><a className={styles.socialLink} href={twitterUrl} target="_blank"><i className="fab fa-twitter"></i></a></h2>
         <h2>Made by:<a href="mailto:ronaldpedid@live.com?subject=web_development"> The Sage Mages</a></h2>
       </footer>
     )
